d8620d11e9422f9c5e59d502c3506d318668a076
User <raresadrians@yahoo.com>
2021-11-01 21:23:53 +0200

    Dummy cartscreen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import { Switch } from 'react-router';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
     <Router>
-    <div className="grid-container">
-    <header className="row">
-      <div>
-        <a className="brand" href="/">amazona</a>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/cart/:id?" component={CartScreen}></Route>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+          </Switch>
+        </main>
+        <footer className="row center">All right reserved</footer>
       </div>
-      <div>
-        <a href="/cart">Cart</a>
-        <a href="/signin">Sign In</a>
-      </div>
-    </header>
-    <main>
-      <Switch>
-        <Route path="/product/:id" component={ProductScreen}></Route>  
-        <Route path="/" component={HomeScreen} exact></Route>
-      </Switch>
-    </main>
-    <footer className="row center">All right reserved</footer>
-  </div>
-  </Router>
+    </Router>
   );
 }
 
