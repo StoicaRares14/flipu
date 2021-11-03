d46af8dd4924ebbf625f513d869765e388367ad5
User <raresadrians@yahoo.com>
2021-11-03 15:33:21 +0200

    signin screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import { useSelector } from "react-redux";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -34,6 +35,7 @@ function App() {
           <Switch>
             <Route path="/cart/:id?" component={CartScreen}></Route>
             <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/signin" component={SigninScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
           </Switch>
         </main>
