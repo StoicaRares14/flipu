05cc4870fe87c93dac5a46204109308af6d65e25
User <raresadrians@yahoo.com>
2021-11-03 22:09:10 +0200

    signin implementation

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { Switch } from "react-router";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
+import { signout } from "./actions/userActions";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const disptach = useDispatch();
+  const signoutHandler = ()=>{
+    disptach(signout());
+  }
   return (
     <Router>
       <div className="grid-container">
@@ -28,7 +34,18 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                    {userInfo.name}<i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
-function SigninScreen() {
+
+function SigninScreen(props) {
     const [email,setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const redirect = props.location.search
+        ? props.location.search.split('=')[1]
+        : '/';    
+
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo, loading, error } = userSignin;
+    
+    const dispatch = useDispatch();
     const submitHandler = (e) =>{
         e.preventDefault();
-        //TODO: sigin action
+        dispatch(signin(email, password));    
     }
+
+    useEffect(() =>{
+        if(userInfo){
+            props.history.push(redirect);
+        }
+    }, [props.history, redirect,userInfo]);
     return (
         <div>
             <form className='form' onSubmit={submitHandler}>
@@ -15,6 +35,8 @@ function SigninScreen() {
                     <h1>
                         Sign In
                     </h1>
+                    {loading && <LoadingBox></LoadingBox>}
+                    {error && <MessageBox variant='danger'>{error}</MessageBox>}
                 </div>
                 <div>
                     <label htmlFor="email">Email adress</label>
